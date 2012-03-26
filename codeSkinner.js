95c83f23c373408650e9e7df15270893b2457859
unknown <PC@PC-PC.(none)>
2012-03-25 23:48:37 -0500

    Added a default template solution, since the template most people would use is very simple.

--- codeSkinner.js
+++ codeSkinner.js
@@ -1,23 +1,35 @@
-var codeSkinner = {
-	init: function(config){
-		this.template = config.template;
-		this.skin();
-	},
-	skin: function(){
-		var template = this.template;
-		
-		$.each($('pre'), function(){
-			var $this = $(this),
-				codeArray = [];
-			$.each($this.text().split('\n'), function(i, line){
-				codeArray[i] = {
-					isOdd: (i%2==0),
-					code: line
-				};
-			})
-			skin = Handlebars.compile(template);
-			$this.replaceWith(skin(codeArray));
-		});
+	var codeSkinner = {
+		init: function(config){
+			if(!config){config = [];}
+			this.template = config.template || this.getDefaultTemplate();
+			console.log();
+			this.skin();
+		},
+		skin: function(){
+			var template = this.template;
+			
+			$.each($('pre'), function(){
+				var $this = $(this),
+					codeArray = [];
+				$.each($this.text().split('\n'), function(i, line){
+					codeArray[i] = {
+						isOdd: (i%2==0),
+						code: line
+					};
+				})
+				skin = Handlebars.compile(template);
+				$this.replaceWith(skin(codeArray));
+			});
+		},
+		getDefaultTemplate: function(){ //store this within a function so it's only created if we need it
+			return "<ol class='code'>" + 
+						"{{#each this}}" + 
+							"<li {{#if isOdd}} class='odd'{{/if}}>" + 
+								"{{code}}" + 
+							"</li>"+
+						"{{/each}}"+
+					"</ol>";
+		}
 	}
-}
+	
 
